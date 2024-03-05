bda6ca7e2dab49e7b5485a46bda73e77e264e93c
prawee wongsa <prawee@hotmail.com>
2024-03-05 15:33:42 +0700

    update encrypt and decrypt mobile

--- src/api/student/content-types/student/lifecycles.js
+++ src/api/student/content-types/student/lifecycles.js
@@ -1,8 +1,23 @@
 const md5 = require('md5')
+const cryptoJs = require('crypto-js')
+const cryptoKey = 'my-secret-key'
+
 module.exports = {
     async beforeCreate(event) {
         // console.log('beforeCreate is worked...', event.params.data)
         // event.params.data.mobile = 'demo-hook'
-        event.params.data.mobile = md5(event.params.data.mobile)
+        // event.params.data.mobile = md5(event.params.data.mobile)
+        const mobile = event.params.data.mobile
+        const makeEncrypt = cryptoJs.AES.encrypt(mobile, cryptoKey).toString()
+        
+        event.params.data.mobile = makeEncrypt
     },
+    async afterFindOne(event) {
+        console.log('afterFindOne ', event.result.mobile)
+
+        const mobile = event.result.mobile
+        const makeDecrypt = cryptoJs.AES.decrypt(mobile, cryptoKey)
+        const makeToString = makeDecrypt.toString(cryptoJs.enc.Utf8)
+        event.result.mobile = makeToString
+    }
 }
