9c52d6140f06cab578f1edf64a6e35e4b81a4c82
prawee wongsa <prawee@hotmail.com>
2024-03-05 16:11:43 +0700

    how to encypt and decrypt data

--- src/api/student/content-types/student/lifecycles.js
+++ src/api/student/content-types/student/lifecycles.js
@@ -2,22 +2,31 @@ const md5 = require('md5')
 const cryptoJs = require('crypto-js')
 const cryptoKey = 'my-secret-key'
 
+const appId = 'api::student.student'
 module.exports = {
     async beforeCreate(event) {
         // console.log('beforeCreate is worked...', event.params.data)
         // event.params.data.mobile = 'demo-hook'
         // event.params.data.mobile = md5(event.params.data.mobile)
         const mobile = event.params.data.mobile
-        const makeEncrypt = cryptoJs.AES.encrypt(mobile, cryptoKey).toString()
+        // const makeEncrypt = cryptoJs.AES.encrypt(mobile, cryptoKey).toString()
         
-        event.params.data.mobile = makeEncrypt
+        event.params.data.mobile = await strapi.service(appId).encrypt(mobile)
     },
     async afterFindOne(event) {
-        console.log('afterFindOne ', event.result.mobile)
+        // console.log('afterFindOne ', event.result.mobile)
 
         const mobile = event.result.mobile
-        const makeDecrypt = cryptoJs.AES.decrypt(mobile, cryptoKey)
-        const makeToString = makeDecrypt.toString(cryptoJs.enc.Utf8)
-        event.result.mobile = makeToString
+        // const makeDecrypt = cryptoJs.AES.decrypt(mobile, cryptoKey)
+        // const makeToString = makeDecrypt.toString(cryptoJs.enc.Utf8)
+        event.result.mobile = await strapi.service(appId).decrypt(mobile)
+    },
+    async afterFindMany(event) {
+        console.log('afterFindMany ', event.result)
+        event?.result?.map(async result => {
+            console.log('result ', result)
+            result.mobile = await strapi.service(appId).decrypt(result.mobile)
+            return result 
+        })
     }
 }
